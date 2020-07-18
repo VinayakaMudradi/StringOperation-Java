1a0f1d38ab547f479baeedac18d22de2f7f978b0
Vinayaka Naik <68412742+VinayakaMudradi@users.noreply.github.com>
2020-07-18 19:51:36 +0530

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -1,8 +1,5 @@
 /******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
+VINAYAKA MUDRADI
 
 *******************************************************************************/
 import java.util.Scanner;
