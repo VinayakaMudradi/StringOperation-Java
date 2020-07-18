3b19bdca3fe2417b6b86cda43a3ed8d97c4c8c57
Vinayaka Naik <68412742+VinayakaMudradi@users.noreply.github.com>
2020-07-18 19:50:13 +0530

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class Main {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        List<String> result = new ArrayList<String>();
+        if(s.length()>k){
+            char arr[] = s.toCharArray();
+           
+            for(int i=0; i<s.length()-1; i++){
+                if(i+k<=s.length()){
+                    result.add(s.substring(i, i+k));
+                }
+            }
+            Collections.sort(result);
+        }
+        smallest = result.get(0);
+        largest = result.get(result.size()-1);
+        return smallest + "\n" + largest;
+        // for(int j=0; j<result.size(); j++){
+        //     System.out.println(result.get(j));
+        // }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        // getSmallestAndLargest(s, k);
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
+
+
